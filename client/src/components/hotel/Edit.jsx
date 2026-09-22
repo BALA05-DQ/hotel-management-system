c1bfc27b3b320343fd28d1afc8a24c7b7c475ad1
Bala S <sbalasbala400@gmail.com>
2026-09-22 12:59:44 +0530

    Fix Edit CSS filename

--- client/src/components/hotel/Edit.jsx
+++ client/src/components/hotel/Edit.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import HotelForm from "./HotelForm";
-import "./addHotel.css";
+import "./AddHotel.css";
 
 function Edit() {
   const navigate = useNavigate();
