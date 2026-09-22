842fac88344ebb106c02ef8a167d1102e4342af6
Bala S <sbalasbala400@gmail.com>
2026-09-22 12:45:56 +0530

    fix AddHotel CSS filename

--- client/src/components/hotel/AddHotel.jsx
+++ client/src/components/hotel/AddHotel.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import HotelForm from "./HotelForm";
-import "./addHotel.css";
+import "./AddHotel.css";
 
 function AddHotel() {
   const navigate = useNavigate();
